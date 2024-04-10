7832ab59e10923a59b286f89110cd9217d767fdd
Marco Parra <mpdragon123@gmail.com>
2024-04-09 21:27:46 -0300

    Create NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+       
+        <Navbar/>
+        <main className='bg-slate-700 h-screen p-16 text-white'>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <main>
+    <div>
       <h1>Next E-Commerce - Artists </h1>
-    </main>
-);
+    </div>
+  );
 }
-
