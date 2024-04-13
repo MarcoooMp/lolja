c8d7ce98ee37fdc7ccc010fa69493c43ef0c9513
Marco Parra <mpdragon123@gmail.com>
2024-04-13 15:30:42 -0300

    Import Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import clsx from 'clsx';
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <ClerkProvider>
+      <html lang="pt-br">
       <body className={clsx(inter.className, 'bg-slate-700')}>
         <Navbar />
         <main className=' h-screen p-16'>{children}</main>
       </body>
-    </html>
+      </html> 
+    </ClerkProvider>
   );
 }
 
